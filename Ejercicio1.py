264752b6e57ce73512d87cbfa2eae5b6e2079e63
Eduardo Burbano <eburbano@unal.edu.co>
2018-09-09 12:13:27 -0500

    Final

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,65 +1,31 @@
-
 # -*- coding: utf-8 -*-
-
 """
-By: Eduardo Burbano
+Created on Sun Sep  9 11:36:18 2018
+
+@author: Eduardo Burbano 
 
 Punto 1
 """
-
 from turtle import *
 
-def cuad(n):
+def cuad(lado):
     for i in range (4):
-        for i in range (2):
-            forward (10)
-            right (90)
-        penup()
-        right(180)
-        forward(100)
-        right(270)
-        pendown()
-    penup()
-    right(90)
-    forward(20)
-    right(90)
-    for i in range(4):
-        for i in range (1):            
-            pendown()            
-            forward(10)
-            right(90)
-            forward(10)
-                          
-        penup()
-        forward(120)
-    penup()
-    forward(10)
-    right(90)
-    forward(10)
-    for i in range(4):
         for i in range(1):
-            pendown()
+            penup()  #Reemplazar por pendown() par mostrar el recorrdo
             forward(10)
             right(90)
-            forward(10)
-        right(270)
-        penup()
-        forward(120)
-        right(90)
-    
-    right(90)
-    penup()
-    forward(20)
-    right(90)
-    for i in range(4):
-        for i in range(2):
             pendown()
             forward(10)
-            right(90)            
-        penup()
-        forward(120)
-        right(270)
-        
-                        
+            for i in range (3):                
+                right(90)
+                pendown()
+                forward(20)
+            
+            right(90)
+            forward(10)
+            right(270)
+        penup() #Reemplazar por pendown() para mostrar el recorrido
+        forward(110)
+        right(90)
 cuad(4)
 done()
